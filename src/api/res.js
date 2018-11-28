fd12a36fe6aa195d136ebc8e441afb9ef5c6c32d
weid <weidong@yinhai.com>
2018-11-28 08:49:15 +0800

    开发选择服务公共页面

---
+++ src/api/res.js
@@ -0,0 +1,114 @@
+import request from '../libs/request';
+
+/**
+ * 获取菜单 Tree
+ */
+export function getMenuTree(params) {
+  return request('/v1/core/res/menutree', {
+    method: 'get',
+    params,
+  });
+}
+
+/**
+ * 编辑菜单
+ * @param {string} id 有ID为修改，反正为新增
+ * @param {object} data 提交数据
+ */
+export function setMenu(id, data) {
+  return request('/v1/core/res/menu', {
+    method: id ? 'put' : 'post',
+    data,
+  });
+}
+
+/**
+ * 删除菜单
+ * @param {string} id 删除目标ID
+ */
+export function deleteMenu(id) {
+  return request('/v1/core/res/menu', {
+    method: 'delete',
+    data: {
+      id,
+    },
+  });
+}
+
+/**
+ * 加载服务分类树
+ */
+export function getApiTypeTree() {
+  return request('/v1/core/res/apitypes', {
+    mehtod: 'get',
+  });
+}
+
+/**
+ * 新增、修改服务类
+ * @param {object} data 提交数据，有 ID 新增，无ID修改
+ */
+export function editApiType(data) {
+  return request('/v1/core/res/apitype', {
+    method: data.id ? 'put' : 'post',
+    data,
+  });
+}
+
+/**
+ * 删除服务分类
+ * @param {string} id 删除类型ID
+ */
+export function deleteApiType(id) {
+  return request('/v1/core/res/apitype', {
+    method: 'delete',
+    data: {
+      id,
+    },
+  });
+}
+
+/**
+ * 查询服务列表
+ * @param {object} params 搜索条件
+ */
+export function getApiListByOption(params) {
+  return request('/v1/core/res/apis', {
+    method: 'get',
+    params,
+  });
+}
+
+/**
+ * 查询服务列表 + 类型 树（全加载）
+ */
+export function getApisTree(params) {
+  return request('/v1/core/res/apisTree', {
+    method: 'get',
+    params,
+  });
+}
+
+/**
+ * 新增服务
+ * @param {object} data 提交、修改数据，有ID 修改，无ID 新增
+ */
+export function editApi(data) {
+  return request('/v1/core/res/api', {
+    method: data.id ? 'put' : 'post',
+    data,
+  });
+}
+
+/**
+ * 删除服务
+ * @param {string} id 删除服务ID
+ */
+export function deleteApi(id) {
+  return request('/v1/core/res/api', {
+    method: 'delete',
+    data: {
+      id,
+    },
+  });
+}
--- src/components/SelectApi/SelectApi.vue
+++ src/components/SelectApi/SelectApi.vue
@@ -1,57 +1,64 @@
 <!-- 选择服务公共页面 -->
 <template>
-  <el-dialog :title='title' :visible='isVisible' width="60%" :before-close="close">
-
+  <el-dialog :title="title" visible width="60%" :before-close="close" :close-on-click-modal="false">
     <div class="search-bar">
       <el-form :inline="true">
         <el-form-item label="服务名称">
-          <el-input v-model.trim="formApi.name" size='mini'></el-input>
+          <el-input v-model.trim="formApi.name" size="mini"></el-input>
         </el-form-item>
 
         <el-form-item label="服务编码">
-          <el-input v-model.trim="formApi.code" size='mini'></el-input>
+          <el-input v-model.trim="formApi.code" size="mini"></el-input>
         </el-form-item>
 
         <el-form-item>
-          <el-button type='primary' size='mini' @click='loadApi'>查询</el-button>
+          <el-button type="primary" size="mini" @click="loadApi(false)">查询</el-button>
         </el-form-item>
 
         <el-form-item>
-          <el-button size='mini' @click="loadApi(true)">重置</el-button>
+          <el-button size="mini" @click="loadApi(true)">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
     <!-- 搜索框 END -->
-    <zk-table
-      v-loading='loading'
-      :columns='columns'
-      :data='treeData'
-      :expandType='false'
+    <el-table
+      :data="treeData"
+      width="100%"
+      highlight-current-row
       border
-      :show-row-hover='true'
-      :isFold='false'
-      :selection-type='true'
-
-      ref='apiTree'>
-    </zk-table>
+      @row-click="onClickCacheRow"
+      v-loading="loading"
+    >
+      <el-table-column label="服务名称">
+        <template slot-scope="scope">
+          <span class="ms-tree-space" v-for="(item,index) in scope.row.level" :key="index"></span>
+          <el-checkbox
+            class="m-5"
+            v-if="multiple"
+            :indeterminate="scope.row.isIndeterminate"
+            v-model="scope.row.isChecked"
+            @change="onChangeChecked(scope.row)"
+          ></el-checkbox>
+          {{scope.row.name}}
+        </template>
+      </el-table-column>
+      <el-table-column label="服务编码" prop="code"></el-table-column>
+      <el-table-column label="服务路径" prop="url"></el-table-column>
+    </el-table>
     <!-- tree grid end -->
-    <span slot="footer" class='dialog-footer'>
-        <el-button @click='close'>取 消</el-button>
-        <el-button type='primary' @click='sendSelect'>确 定</el-button>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="close">取 消</el-button>
+      <el-button type="primary" @click="sendSelect">确 定</el-button>
     </span>
   </el-dialog>
 </template>
 
 <script>
-import ZkTable from 'vue-table-with-tree-grid';
 import * as Utils from '@/libs/utils';
-import * as resApi from '@/api/resources';
+import * as resApi from '@/api/res';
 
 export default {
   name: 'SelectApi',
-  components: {
-    ZkTable,
-  },
   props: {
     title: {
       type: String,
@@ -63,66 +70,104 @@ export default {
     },
     selectedIds: {
       type: Array,
+      default() {
+        return [];
+      },
     },
   },
   data() {
     return {
-      isVisible: true,
       loading: false,
-      columns: [
-        {
-          label: '服务名称',
-          prop: 'name',
-        },
-        {
-          label: '服务编码',
-          prop: 'code',
-          width: 200,
-        },
-        {
-          label: '服务路径',
-          prop: 'url',
-        },
-      ],
       apiList: [],
       formApi: {
         name: '',
         code: '',
       },
+      cacheCurRow: {}, // 单选缓存选中行
     };
   },
   computed: {
+    defaultSelectMap() {
+      const defaultSelectMap = {};
+
+      this.selectedIds.forEach(item => {
+        defaultSelectMap[item] = true;
+      });
+
+      return defaultSelectMap;
+    },
+    // 组装树结构
     treeData() {
-      return Utils.data2treeArr([...this.apiList], 'id', 'pid');
+      const returnTree = Utils.data2treeGridArr(this.apiList, 'id', 'pid', true);
+
+      returnTree.forEach(item => {
+        if (item.children && item.children.length) {
+          this.$set(item, 'isIndeterminate', false); // 设置父节点办选状态
+        }
+        this.$set(item, 'isChecked', false);
+      });
+
+      // ! 默认选中
+      if (this.apiList.length) {
+        this.$nextTick(() => {
+          returnTree.forEach(item => {
+            if (this.defaultSelectMap[item.id]) {
+              item.isChecked = true;
+              this.setChildChecked(item.children, true);
+              this.setParentsChecked(item.parent);
+            }
+          });
+        });
+      }
+      return returnTree;
     },
   },
+
   created() {
     this.loadApi();
   },
 
   methods: {
-    // 获取服务列表
+    /**
+     * 获取服务列表
+     */
     loadApi(isClear) {
       if (isClear) {
         this.formApi = {};
       }
       this.loading = true;
-      resApi.getApiListByOption(this.formApi).then(res => {
+      resApi.getApisTree(this.formApi).then(res => {
         this.loading = false;
-        this.apiList = res;
+        // 没查询到数据时，后端返回 null
+        this.apiList = res || [];
       });
     },
-    sendSelect() {
-      const apiMap = {};
-      const selectedIds = this.$refs.apiTree.getCheckedProp('id');
-      const sendData = [];
-      this.$refs.apiTree.bodyData.forEach(item => {
-        apiMap[item.id] = item;
-      });
 
-      selectedIds.forEach(id => {
-        sendData.push(apiMap[id]);
-      });
+    /**
+     * 向父组件发送选中数据
+     */
+    sendSelect() {
+      let sendData = [];
+      if (this.multiple) {
+        // ! 后期优化，多选时可配置返回值
+        // 只返回选中的子类
+        this.apiList.forEach(item => {
+          if (item.isChecked && item.children === undefined) {
+            sendData.push(item);
+          }
+        });
+      } else {
+        // 只能选择目标服务
+        if (this.cacheCurRow.children && this.cacheCurRow.children.length) {
+          this.$message({
+            message: '请正确选择系统服务',
+            type: 'warning',
+          });
+          return;
+        }
+        sendData = this.cacheCurRow;
+      }
+
       // 返回完整对象
       this.$emit('select', sendData);
       this.close();
@@ -130,8 +175,80 @@ export default {
     close() {
       this.$emit('close');
     },
+
+    /**
+     * checkBox状态发生变化时
+     */
+    onChangeChecked(row) {
+      // 设置子孙勾选状态
+      this.setChildChecked(row.children, row.isChecked);
+
+      // 点击root节点
+      if (row.isIndeterminate !== undefined && row.parent === undefined) {
+        row.isIndeterminate = false;
+      } else {
+        this.setParentsChecked(row.parent);
+      }
+    },
+
+    /**
+     * 设置子孙选中状态
+     */
+    setChildChecked(children, isCheck) {
+      if (children === undefined || children.lenght === 0) return;
+      children.forEach(item => {
+        item.isChecked = isCheck;
+        if (item.isIndeterminate && isCheck === false) {
+          item.isIndeterminate = false;
+        }
+        this.setChildChecked(item.children, isCheck);
+      });
+    },
+
+    /**
+     * 设置父、爷选中状态
+     */
+    setParentsChecked(father) {
+      while (father) {
+        if (father.children && father.children.length) {
+          const isCheck = father.children.every(item => item.isChecked === true);
+
+          father.isChecked = isCheck;
+          // 如果全选中或没选中项，半选状态不显示，有选中项 显示半选状态
+          father.isIndeterminate =
+            father.children.find(item => item.isChecked === true) === undefined ? false : !isCheck;
+        }
+
+        father = father.parent;
+      }
+    },
+
+    /**
+     * 单选时保存当前选中行
+     */
+    onClickCacheRow(row) {
+      if (this.multiple) return;
+      this.cacheCurRow = row;
+    },
   },
 };
 </script>
 <style lang='less' scoped>
+.m-5 {
+  margin: 0 5px;
+}
+.ms-tree-space {
+  position: relative;
+  top: 1px;
+  display: inline-block;
+  font-family: 'Glyphicons Halflings';
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1;
+  width: 18px;
+  height: 14px;
+}
+.ms-tree-space::before {
+  content: '';
+}
 </style>
